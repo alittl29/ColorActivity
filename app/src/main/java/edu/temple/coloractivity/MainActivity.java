9d7a582c0b8f68faf88ecd634ad464ab493a6b6b
anthonylittlejohn <tuh40168@temple.edu>
2020-09-29 15:22:13 -0400

    3rd commit - background changes to match selected color

--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -2,6 +2,7 @@ package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     TextView titleText;
     ArrayList<String> colors;
     ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +53,47 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 titleText.setText(parent.getItemAtPosition(position).toString());
 
+                View myLayout = findViewById(R.id.myLayout);
+                if(listView.getItemAtPosition(position).equals("red"))
+                {
+                    myLayout.setBackgroundColor(Color.RED);
+                }
+                else if (listView.getItemAtPosition(position).equals("blue"))
+                {
+                    myLayout.setBackgroundColor(Color.BLUE);
+                }
+                else if (listView.getItemAtPosition(position).equals("yellow"))
+                {
+                    myLayout.setBackgroundColor(Color.YELLOW);
+                }
+                else if (listView.getItemAtPosition(position).equals("gray"))
+                {
+                    myLayout.setBackgroundColor(Color.GRAY);
+                }
+                else if (listView.getItemAtPosition(position).equals("green"))
+                {
+                    myLayout.setBackgroundColor(Color.GREEN);
+                }
+                else if (listView.getItemAtPosition(position).equals("dark gray"))
+                {
+                    myLayout.setBackgroundColor(Color.DKGRAY);
+                }
+                else if (listView.getItemAtPosition(position).equals("light gray"))
+                {
+                    myLayout.setBackgroundColor(Color.LTGRAY);
+                }
+                else if (listView.getItemAtPosition(position).equals("white"))
+                {
+                    myLayout.setBackgroundColor(Color.WHITE);
+                }
+                else if (listView.getItemAtPosition(position).equals("cyan"))
+                {
+                    myLayout.setBackgroundColor(Color.CYAN);
+                }
+                else if (listView.getItemAtPosition(position).equals("black"))
+                {
+                    myLayout.setBackgroundColor(Color.BLACK);
+                }
             }
         });
 
